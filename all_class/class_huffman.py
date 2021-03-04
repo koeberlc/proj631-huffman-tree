4936fb60b422eb81894aec3b0442cf0f7dfc21c0
esteban143 <40337451+esteban143@users.noreply.github.com>
2021-03-04 19:10:05 +0100

    Add a class File to read or write into files

--- all_class/class_huffman.py
+++ all_class/class_huffman.py
@@ -1,5 +1,11 @@
+from all_class.class_file import File
+
 class Huffman:
 
 	def compress(self, path):
 		print(path)
-		pass
+		file_to_compress = File(path)
+		text_to_compress = file_to_compress.read()
+		print(text_to_compress)
+		file_to_compress.write("test\ntest2")
+		
--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-import all_class.class_constant as const
-import all_class.class_huffman as huffman
+from all_class.class_constant import Constant
+from all_class.class_huffman import Huffman
 
 #Initialisation :
-constant = const.Constant()
+constant = Constant()
 constant.addElement("to_text_to_convert","text/text_to_convert/")
 constant.addElement("to_text_converted","text/text_converted/")
 constant.addElement("to_lexicon","text/lexicon/")
 
-huffman_tree = huffman.Huffman()
+huffman_tree = Huffman()
+
+
+
+#Compression
+file = "bonjour"
+
+path = constant.getElement("to_text_to_convert") + file + ".txt"
+
+huffman_tree.compress(path)
