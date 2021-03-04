35010f116e78fcf532cc6097a27e830fb4c4ba3a
esteban143 <40337451+esteban143@users.noreply.github.com>
2021-03-04 19:23:38 +0100

    Add a class Frequence to make a list of tuple : character, frequency

---
+++ all_class/class_frequence.py
@@ -0,0 +1,24 @@
+class Frequence:
+	
+	def get_frequence(text):
+		#Create a array like [[characters],[frequences]]
+		lex = [[],[]]
+
+		for c in text:
+			if not c in lex[0]:
+				lex[0].append(c)
+				lex[1].append(1)
+
+			else:
+				index = lex[0].index(c)
+				lex[1][index]+=1
+
+		#Create a new array like [[char1,freq1],[char2,freq2]]
+		newLex = []
+		for i in range(len(lex[0])):
+			newLex.append((lex[0][i],lex[1][i]))
+		
+		#Sort the array by frequency then by character
+		lex.sort(key=itemgetter(1,0))
+
+		return lex
--- all_class/class_huffman.py
+++ all_class/class_huffman.py
@@ -1,11 +1,17 @@
 from all_class.class_file import File
+from all_class.class_frequence import Frequence
 
 class Huffman:
 
 	def compress(self, path):
-		print(path)
+		
 		file_to_compress = File(path)
 		text_to_compress = file_to_compress.read()
-		print(text_to_compress)
-		file_to_compress.write("test\ntest2")
 		
+		frequence = Frequence.get_frequence(text_to_compress)
+
+		#Tests
+		print(path)
+		print(text_to_compress)
+		print(frequence)
+	
