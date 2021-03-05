290af450dedb4001b84651d25a2a8921834fd12a
CAULLIREAU Dorian <54810120+caullird@users.noreply.github.com>
2021-03-05 17:15:09 +0100

    Update

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
-from main_program.constant import Constant
+
 from main_program.huffman import Huffman
 
-# Initialisation :
-constant = Constant()
-
-constant.addElement("to_text_to_convert", "text/text_to_convert/")
-constant.addElement("to_text_converted", "text/text_converted/")
-constant.addElement("to_lexicon", "text/lexicon/")
-
 huffman_algo = Huffman()
-
-
-
 # Compression
 file = "bonjour"
 
-huffman_algo.compress(constant, file)
+huffman_algo.compress( file)
 
 
 # all_class -> main_prog -> point commun (pour faire un dossier)
